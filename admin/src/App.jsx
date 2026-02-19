4e40c55f015ac2e8e939622e5ff142d894258544
Herval Mata <hervalmataamparo@gmail.com>
2026-02-19 17:53:17 -0300

    :sparkles: feat: expo ecommerce - setup dashboard
    Signed-off-by: Herval Mata <hervalmataamparo@gmail.com>

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,23 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
+import { useAuth } from "@clerk/clerk-react"
+import PageLoader from "./components/PageLoader";
+import { Route, Routes } from "react-router";
 
 function App() {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) return <PageLoader />
 
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <Routes>
+      <Route path="/login" element={isSignedIn ? <Navigate to={"/dashboard"} /> : <LoginPage />} />
+      <Route path="/" element={isSignedIn ? <DashboardLayout /> : <Navigate to={"/login"} />}>
+        <Route index element={<Navigate to={"/dashboard"} />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="customers" element={<CustomersPage />} />
+      </Route>
+    </Routes>
   )
 }
 
